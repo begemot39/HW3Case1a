40e730dce2ee8586ea217dc652b8dd6ad40ed4d5
Vitaliy K. <begemot39@mail.ru>
2024-08-21 11:13:45 +0300

    Edits2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,6 @@ public class BonusMilesService {
     public int calculate(int price) {
         int ratio = 20; // Ставка бонусных миль.
         int bonusMiles = price / ratio;
-        ;
         return bonusMiles;
     }
 }
